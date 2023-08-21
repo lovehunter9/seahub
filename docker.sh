5f8ab6e674415ffd61f0278b8a5cc1e4e7b7e8f5
Wang Rongxiang <wangrongxiang@bytetrade.io>
2023-08-21 19:51:26 +0800

    try to lower npm memory cost

--- docker.sh
+++ docker.sh
@@ -7,7 +7,7 @@ cd frontend
 #npm install @babel/plugin-proposal-private-property-in-object caniuse-lite chart.js@^3.9.1 react-chartjs-2@^4.3.1 --legacy-peer-deps --save-dev
 #npm run build
 export DANGEROUSLY_DISABLE_HOST_CHECK=true
-#export NODE_OPTIONS="--max_old_space_size=1024 --openssl-legacy-provider"
+export NODE_OPTIONS="--max_old_space_size=1024 --openssl-legacy-provider"
 npm run dev --allowed-hosts all --allowed-host .snowinning.com &
 cd ..
 
