d08c7b23e61b9ed19d23aea9c96e1d88d60a9ad8
Wang Rongxiang <wangrongxiang@bytetrade.io>
2023-08-22 11:22:23 +0800

    extreme test v2

--- docker.sh
+++ docker.sh
@@ -3,17 +3,5 @@
 #
 #
 
-#cd frontend
-##npm install @babel/plugin-proposal-private-property-in-object caniuse-lite chart.js@^3.9.1 react-chartjs-2@^4.3.1 --legacy-peer-deps --save-dev
-##npm run build
-##export DANGEROUSLY_DISABLE_HOST_CHECK=true
-##export NODE_OPTIONS="--max_old_space_size=512 --openssl-legacy-provider"
-##npm run dev --allowed-hosts all --allowed-host .snowinning.com &
-#live-server --port=3000 &
-#cd ..
-
 seaf-server -c ~/dev/conf -d ~/dev/seafile-data -D all -f -l - &
-#cd frontend
-#npm run dev &
-#cd ..
 python3 manage.py runserver 0.0.0.0:8000
